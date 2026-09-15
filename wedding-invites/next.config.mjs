f8ad0af57ce934b3a5c98c710fd3c352ea66c4d4
brahimbbr312 <153216199+brahimbbr312@users.noreply.github.com>
2026-09-15 18:45:12 +0100

    Remove Next.js configuration

--- wedding-invites/next.config.mjs
+++ wedding-invites/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: '/wedding-invites',
+  assetPrefix: '/wedding-invites/',
 };
 
-export default nextConfig;
+module.exports = nextConfig;

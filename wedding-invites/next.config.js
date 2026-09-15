75916b517ff16dccf75fdff74e33bda27224e78a
brahimbbr312 <153216199+brahimbbr312@users.noreply.github.com>
2026-09-15 18:39:57 +0100

    Rename wedding project folder and configure GitHub Pages

---
+++ wedding-invites/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: '/wedding-invites',
+  assetPrefix: '/wedding-invites/',
+};
+
+module.exports = nextConfig;

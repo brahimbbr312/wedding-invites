c2dd27afdc3f576e3e48fe93d713f3a29f95d0f0
brahimbbr312 <153216199+brahimbbr312@users.noreply.github.com>
2026-09-15 18:59:22 +0100

    layout section

--- wedding-invites/app/layout.js
+++ wedding-invites/app/layout.js
@@ -33,7 +33,7 @@ export const metadata = {
 };
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="fr" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body>{children}</body>
     </html>
   );
